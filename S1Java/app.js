3df9d355c4ef90c5f877610fc6491c3523cbbcae
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2021-11-17 22:44:26 -0600

    Varios

---
+++ S1Java/app.js
@@ -0,0 +1,47 @@
+/*
+console.log('Hola desde js')
+
+/* Palabrs reservadas: let, var y const
+ */
+/*
+let age = 0 //decalaracion y asignado un valor
+console.log('Edad:', age)
+age = 25
+console.log('Edad:', age)
+
+/* Buena practica */
+/*
+let Lastname = ''
+console.log('Apellido:', Lastname) 
+Lastname = 'Gutierrez Manriquez'
+console.log('Apellido:', Lastname)
+
+const pi = 2.1416
+// pi = 2.1416 -> no se le puede reasignar un valor
+
+// == ===
+var number1 = 10
+var number2 = '10'
+
+console.log(number1 == number2) //verdadero porque solo compara el valor
+console.log(number1 === number2) //falso porque compara otro tipo de dato
+
+console.log(number1 != number2) //Falso porque solo compara el valor y si son iguales 
+console.log(number1 !== number2) //Verdadero porque si son diferentes en el tipo de dato, aunque sean iguales
+*/
+
+let firstname
+firstname = 'Erik'
+console.log('Nombre: ', firstname)
+
+let Lastname
+Lastname = 'Gutierrez Manriquez'
+console.log('Apellidos: ', Lastname)
+
+let age
+age = '28'
+console.log('Edad: ', age)
+
+let job
+job = 'Ingeniero de Servicio'
+console.log('Trabajo/Descripción: ', job)
